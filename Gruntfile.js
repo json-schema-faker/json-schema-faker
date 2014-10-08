687ed353c3f38ff82d91e2160de99f47b3e9f3db
Alvaro Cabrera <pateketrueke@gmail.com>
2014-10-08 10:47:26 -0500

    Tasks for development

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    jasmine_node: {
+      all: ['spec'],
+      options: {
+        coffee: true,
+        useHelpers: true,
+        includeStackTrace: false
+      }
+    },
+    eslint: {
+      all: ['lib/**/*.js']
+    },
+    watch: {
+      all: {
+        files: ['lib/**/*.js', 'spec/**/*.coffee'],
+        tasks: ['eslint', 'jasmine_node']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-eslint');
+};
