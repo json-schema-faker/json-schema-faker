7c6d5dfde27be6417632db07857baa625d64c7ec
Alvaro Cabrera <pate@patech.local>
2014-12-18 12:50:25 -0600

    Main script file

---
+++ main.js
@@ -0,0 +1,13 @@
+var $ = require('./bower_components/jquery/dist/jquery.js');
+
+var jsf = require('json-schema-faker');
+
+var input = $('#input_schema'),
+    output = $('#output_sample'),
+    generate = $('#generate_sample');
+
+generate.on('click', function() {
+  var schema = JSON.parse(input.val());
+
+  output.val(JSON.stringify(jsf(schema), null, 2));
+})
