0297fdfbf7e17f6d2649966088eab84d7b878d58
Alvaro Cabrera <pateketrueke@gmail.com>
2016-02-03 03:12:06 -0600

    Fixed bundling; enable locales support

--- build-dist.js
+++ build-dist.js
@@ -19,21 +19,23 @@ var b = browserify({
 });
 
 // unfortunately standalone didn't work
-var exposeName = 'jsf';
+var exposeName = 'jsf',
+    defaultLocale = 'en';
 
 b.add(path.join(__dirname, 'lib/index.js'), { expose: pkg.name, entry: true });
 
-// this way we can mock locales and minimal stubs
-b.require([
-  { file: path.join(__dirname, 'stubs/faker.js'), expose: 'faker' },
-  { file: path.join(__dirname, 'stubs/chance.js'), expose: 'chance' }
-]);
+// this way we can build all locales?
+b.require(path.join('faker/locale', defaultLocale), { expose: 'faker' });
+
+// disable chance by default?
+b.require(path.join(__dirname, 'stubs/chance.js'), { expose: 'chance' });
 
 b.bundle(function(err, buffer) {
   if (err) {
     throw err;
   }
 
+  // hack the window.jsf variable (we need another kind of export for cdnjs?)
   fs.writeFileSync(destFile, banner + '(function(){' + 'var ' + buffer.toString().trim()
-    + ';window.' + exposeName + '=require("' + pkg.name + '")})();');
+    + ';\nwindow.' + exposeName + '=require("' + pkg.name + '")})();');
 });
