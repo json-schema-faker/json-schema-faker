ea375275f2b2a1fad5ed2516ddfeb73efd0fb49d
Tomasz Ducin <tomasz.ducin@gmail.com>
2016-07-31 21:16:51 +0200

    improved docs

--- ts/generators/coreFormat.ts
+++ ts/generators/coreFormat.ts
@@ -1,6 +1,10 @@
 import container = require('../class/Container');
 var randexp = container.get('randexp');
 
+/**
+ * Predefined core formats
+ * @type {[key: string]: string}
+ */
 var regexps: IStringMap = {
   email: '[a-zA-Z\\d][a-zA-Z\\d-]{1,13}[a-zA-Z\\d]@{hostname}',
   hostname: '[a-zA-Z]{1,33}\\.[a-z]{2,4}',
