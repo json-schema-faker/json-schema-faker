19fcbb49c6acfc11ceb196ba05527aa9c5cf83b2
Alvaro Cabrera <pateketrueke@gmail.com>
2016-11-24 14:04:03 -0600

    Copy clean() impl to .ts; fix

---
+++ ts/core/clean.ts
@@ -0,0 +1,57 @@
+// TODO: tsify
+
+function isArray(obj) {
+  return obj && Array.isArray(obj);
+}
+
+function isObject(obj) {
+  return obj && typeof obj === 'object';
+}
+
+function hasNothing(obj) {
+  return typeof obj === 'undefined' || obj === '';
+}
+
+function removeProps(obj, key?, parent?) {
+  var i,
+      value,
+      isFullyEmpty = true;
+
+  if (isArray(obj)) {
+    for (i = 0; i < obj.length; ++i) {
+      value = obj[i];
+
+      if (isObject(value)) {
+        isFullyEmpty = false;
+        removeProps(value, i, obj);
+      } else if (hasNothing(value)) {
+        obj.splice(i--, 1);
+      } else {
+        isFullyEmpty = false;
+      }
+    }
+  } else {
+    for (i in obj) {
+      value = obj[i];
+
+      if (isObject(value)) {
+        isFullyEmpty = false;
+        removeProps(value, i, obj);
+      } else if (hasNothing(value)) {
+        delete obj[i];
+      } else {
+        isFullyEmpty = false;
+      }
+    }
+  }
+
+  if (typeof key !== 'undefined' && isFullyEmpty) {
+    delete parent[key];
+    removeProps(obj);
+  }
+}
+
+export = function(obj: any) {
+  removeProps(obj);
+  return obj;
+};
