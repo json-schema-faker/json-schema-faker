e2f20560513ced0ca1ac38688a854f45fcb86139
Alvaro Cabrera <pateketrueke@gmail.com>
2017-02-25 02:26:41 -0600

    Fix: normalize general strategy for custom generators; see #251

--- ts/class/Container.ts
+++ ts/class/Container.ts
@@ -1,3 +1,44 @@
+// dynamic proxy for custom generators
+function proxy(gen) {
+  return (value) => {
+    var fn = value;
+    var args = [];
+
+    // support for nested object, first-key is the generator
+    if (typeof value === 'object') {
+      fn = Object.keys(value)[0];
+
+      // treat the given array as arguments,
+      if (Array.isArray(value[fn])) {
+        // if the generator is expecting arrays they should be nested, e.g. `[[1, 2, 3], true, ...]`
+        args = value[fn];
+      } else {
+        args.push(value[fn]);
+      }
+    }
+
+    // support for keypaths, e.g. "internet.email"
+    var props = fn.split('.');
+
+    // retrieve a fresh dependency
+    var ctx = gen();
+
+    while (props.length > 1) {
+      ctx = ctx[props.shift()];
+    }
+
+    // retrieve last value from context object
+    value = typeof ctx === 'object' ? ctx[props[0]] : ctx;
+
+    // invoke dynamic generators
+    if (typeof value === 'function') {
+      value = value.apply(ctx, args);
+    }
+
+    return value;
+  };
+}
+
 type Dependency = any;
 
 /**
@@ -33,6 +74,11 @@ class Container {
    */
   public extend(name: string, callback: Function): void {
     this.registry[name] = callback(this.registry[name]);
+
+    // built-in proxy (can be overridden)
+    if (!this.support[name]) {
+      this.support[name] = proxy(() => this.registry[name]);
+    }
   }
 
   /**
