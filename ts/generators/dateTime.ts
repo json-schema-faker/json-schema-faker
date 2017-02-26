5109dc5263d83d20af9b441cf0c029cb0b399864
Alvaro Cabrera <pateketrueke@gmail.com>
2017-02-25 20:15:08 -0600

    Avoid extreme future dates for built-in datetime format; fix #180

--- ts/generators/dateTime.ts
+++ ts/generators/dateTime.ts
@@ -1,12 +1,19 @@
 import random from '../core/random';
 
+const MOST_NEAR_DATETIME = 2524608000000;
+
 /**
  * Generates randomized date time ISO format string.
  *
  * @returns {string}
  */
 function dateTimeGenerator(): string {
-  return new Date(random.number(0, 100000000000000)).toISOString();
+  var date = new Date();
+  var days = random.number(-1000, MOST_NEAR_DATETIME);
+
+  date.setTime(date.getTime() - days);
+
+  return date.toISOString();
 }
 
 export default dateTimeGenerator;
