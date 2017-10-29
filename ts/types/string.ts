b69460a64bf6d276803d7b5fd44b12d12987c506
Alvaro Cabrera <pateketrueke@gmail.com>
2017-10-29 13:07:17 -0600

    Fixed typecast() helper for all-value normalization

--- ts/types/string.ts
+++ ts/types/string.ts
@@ -42,36 +42,17 @@ function generateFormat(value: IStringSchema, invalid: () => string): string {
 var stringType: FTypeGenerator = function stringType(value: IStringSchema): string {
   var output: string;
 
-  var minLength = value.minLength;
-  var maxLength = value.maxLength;
-
-  if (optionAPI('maxLength')) {
-    // Don't allow user to set max length above our maximum
-    if (maxLength && maxLength > optionAPI('maxLength')) {
-      maxLength = optionAPI('maxLength');
+  output = utils.typecast(value, opts => {
+    if (value.format) {
+      return generateFormat(value, () => thunk(opts.minLength, opts.maxLength) );
     }
 
-    // Don't allow user to set min length above our maximum
-    if (minLength && minLength > optionAPI('maxLength')) {
-      minLength = optionAPI('maxLength');
+    if (value.pattern) {
+      return utils.randexp(value.pattern);
     }
-  }
 
-  if (value.format) {
-    output = generateFormat(value, () => thunk(minLength, maxLength) );
-  } else if (value.pattern) {
-    output = utils.randexp(value.pattern);
-  } else {
-    output = thunk(minLength, maxLength);
-  }
-
-  while (output.length < minLength) {
-    output += optionAPI('random')() > 0.7 ? thunk() : utils.randexp('.+');
-  }
-
-  if (output.length > maxLength) {
-    output = output.substr(0, maxLength);
-  }
+    return thunk(opts.minLength, opts.maxLength);
+  });
 
   return output;
 };
