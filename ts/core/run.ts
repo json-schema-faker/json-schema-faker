4b7893cec100ae557af8a4b3e21153146f73dde1
Alvaro Cabrera <pateketrueke@gmail.com>
2017-10-29 22:02:32 -0600

    Basic support for json-path expressions; fix #11

--- ts/core/run.ts
+++ ts/core/run.ts
@@ -1,13 +1,91 @@
+import optionAPI from '../api/option';
 import traverse from './traverse';
 import random from './random';
 import utils from './utils';
 
 import deref from 'deref';
+import jsonpath from 'jsonpath-plus';
+
+function pick(data) {
+  return Array.isArray(data)
+    ? random.pick(data)
+    : data;
+}
+
+function cycle(data, reverse) {
+  if (!Array.isArray(data)) {
+    return data;
+  }
+
+  const value = reverse
+    ? data.pop()
+    : data.shift();
+
+  if (reverse) {
+    data.unshift(value);
+  } else {
+    data.push(value);
+  }
+
+  return value;
+}
+
+function resolve(obj, data, values, property) {
+  if (!obj || typeof obj !== 'object') {
+    return obj;
+  }
+
+  if (!values) {
+    values = {};
+  }
+
+  if (!data) {
+    data = obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(x => resolve(x, data, values, property));
+  }
+
+  if (obj['json-path']) {
+    const params = typeof obj['json-path'] !== 'object'
+      ? { path: obj['json-path'] }
+      : obj['json-path'];
+
+    params.group = obj.group || params.group || property;
+    params.cycle = obj.cycle || params.cycle || false;
+    params.reverse = obj.reverse || params.reverse || false;
+
+    const key = `${params.group}__${params.path}`;
+
+    if (!values[key]) {
+      values[key] = jsonpath({
+        path: params.path,
+        json: data,
+        wrap: false,
+        flatten: true,
+        preventEval: true,
+      });
+    }
+
+    if (params.cycle || params.reverse) {
+      return cycle(values[key], params.reverse);
+    }
+
+    return pick(values[key]);
+  }
+
+  Object.keys(obj).forEach(k => {
+    obj[k] = resolve(obj[k], data, values, k);
+  });
+
+  return obj;
+}
 
 // TODO provide types
 function run(refs: any, schema: JsonSchema, container: Container) {
   try {
-    return traverse(schema, [], function reduce(sub, maxReduceDepth) {
+    const result = traverse(schema, [], function reduce(sub, maxReduceDepth) {
       if (typeof maxReduceDepth === 'undefined') {
         maxReduceDepth = random.number(1, 3);
       }
@@ -95,6 +173,12 @@ function run(refs: any, schema: JsonSchema, container: Container) {
 
       return container.wrap(sub);
     });
+
+    if (optionAPI('resolveJsonPath')) {
+      return resolve(result);
+    }
+
+    return result;
   } catch (e) {
     if (e.path) {
       throw new Error(e.message + ' in ' + '/' + e.path.join('/'));
