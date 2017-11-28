480e68b3b37ce578c628a02fd41f632d11765242
Alvaro Cabrera <pateketrueke@gmail.com>
2017-11-28 13:20:10 -0600

    Missing quotes for ui-reference; fix

--- ts/generators/coreFormat.ts
+++ ts/generators/coreFormat.ts
@@ -8,7 +8,8 @@ var regexps: IStringMap = {
   email: '[a-zA-Z\\d][a-zA-Z\\d-]{1,13}[a-zA-Z\\d]@{hostname}',
   hostname: '[a-zA-Z]{1,33}\\.[a-z]{2,4}',
   ipv6: '[a-f\\d]{4}(:[a-f\\d]{4}){7}',
-  uri: '[a-zA-Z][a-zA-Z0-9+-.]*'
+  uri: 'https?://[a-zA-Z][a-zA-Z0-9+-.]*',
+  'uri-reference': '(https?://|#|/|)[a-zA-Z][a-zA-Z0-9+-.]*',
 };
 
 /**
