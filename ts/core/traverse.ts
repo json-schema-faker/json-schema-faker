18adb461d50fdf569e7fca6d577eccd9bae1bcd7
Alvaro Cabrera <pateketrueke@gmail.com>
2018-04-04 14:38:47 -0500

    Fixed typo

--- ts/core/traverse.ts
+++ ts/core/traverse.ts
@@ -19,7 +19,7 @@ function traverse(schema: JsonSchema, path: SchemaPath, resolve: Function, rootS
   }
 
   if (schema.not && typeof schema.not === 'object') {
-    schema = utils.notValue(sub.not);
+    schema = utils.notValue(schema.not);
   }
 
   if (Array.isArray(schema.enum)) {
