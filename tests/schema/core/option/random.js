daccb96c7ff05f343d404a42918d2f6ceea6c5c1
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-04-25 21:38:48 -0500

    Just test jsf.option(K, V); cleanup

--- tests/schema/core/option/random.js
+++ tests/schema/core/option/random.js
@@ -2,8 +2,6 @@ const seedrandom = require('seedrandom');
 
 module.exports = {
   register(jsf) {
-    return jsf.option({
-      random: seedrandom('some seed'),
-    });
+    return jsf.option('random', seedrandom('some seed'));
   },
 };
