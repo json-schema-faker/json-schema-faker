7f1bae76f41d7fba565525d915030e046790e591
System Administrator <root@c02r6a16g8wp.us.int.genesyslab.com>
2019-06-19 14:07:11 -0400

    Fixing up merge conflicts

--- dist/index.js
+++ dist/index.js
@@ -1080,18 +1080,12 @@ function objectType(value, path, resolve, traverseCallback) {
   var propertyKeys = Object.keys(properties);
   var patternPropertyKeys = Object.keys(patternProperties);
   var optionalProperties = propertyKeys.concat(patternPropertyKeys).reduce(function (_response, _key) {
-<<<<<<< HEAD
     if (requiredProperties.indexOf(_key) === -1) {
        if(_key)
       _response.push(_key); }
     return _response;
   }, []);
   //console.log(properties);
-=======
-    if (requiredProperties.indexOf(_key) === -1) { _response.push(_key); }
-    return _response;
-  }, []);
->>>>>>> b86fb26ca822d31833b91b1a1f07b0234bf9bd2c
   var allProperties = requiredProperties.concat(optionalProperties);
   var additionalProperties = allowsAdditional // eslint-disable-line
   ? value.additionalProperties === true ? anyType : value.additionalProperties : value.additionalProperties;
@@ -1100,8 +1094,7 @@ function objectType(value, path, resolve, traverseCallback) {
     // just nothing
     return {};
   }
-<<<<<<< HEAD
-  // adds the required properties 
+  // adds the required properties
   requiredProperties.forEach(function (key) {
     if (properties[key]) {
       props[key] = properties[key];
@@ -1109,17 +1102,13 @@ function objectType(value, path, resolve, traverseCallback) {
   });
  optionalProperties.forEach(function(key){
    if(properties[key]){
-      props[key + " (optional)"] = properties[key]; // adds the optional field to these values, which will later be used to create the description 
+      props[key + " (optional)"] = properties[key]; // adds the optional field to these values, which will later be used to create the description
    }
   });
   return traverseCallback(props, path.concat(['properties']), resolve);
-    
-// only needed when you are schema faking the values, not needed for what we are doing 
-  /*if (optionAPI('requiredOnly') === true) { 
-=======
 
-  if (optionAPI('requiredOnly') === true) {
->>>>>>> b86fb26ca822d31833b91b1a1f07b0234bf9bd2c
+// only needed when you are schema faking the values, not needed for what we are doing
+  /*if (optionAPI('requiredOnly') === true) {
     requiredProperties.forEach(function (key) {
       if (properties[key]) {
         props[key] = properties[key];
@@ -1151,13 +1140,8 @@ function objectType(value, path, resolve, traverseCallback) {
   var extraProperties = optionalProperties.filter(function (_item) {
     return extraPropertiesRandomOrder.indexOf(_item) !== -1;
   }); // properties are read from right-to-left
-<<<<<<< HEAD
-  
-  var _props = requiredProperties.concat(extraProperties).slice(0, max); //Making a change so that all the properties will be listed with optional ones being shown as optional
-=======
 
-  var _props = requiredProperties.concat(extraProperties).slice(0, max);
->>>>>>> b86fb26ca822d31833b91b1a1f07b0234bf9bd2c
+  var _props = requiredProperties.concat(extraProperties).slice(0, max); //Making a change so that all the properties will be listed with optional ones being shown as optional
 
   var _defns = [];
 
@@ -1317,10 +1301,7 @@ function objectType(value, path, resolve, traverseCallback) {
   }
 
   return traverseCallback(props, path.concat(['properties']), resolve);
-<<<<<<< HEAD
   */
-=======
->>>>>>> b86fb26ca822d31833b91b1a1f07b0234bf9bd2c
 }
 
 /**
@@ -1603,17 +1584,16 @@ function traverse(schema, path, resolve, rootSchema) {
   }
 
   var copy = {};
-<<<<<<< HEAD
    copy.description = "";
   if (Array.isArray(schema)) {
     copy = [];
   }
-  
+
   Object.keys(schema).forEach(function (prop) {
     var schemadescription;
-    //if there is a description we set it otherwise we want an empty string 
+    //if there is a description we set it otherwise we want an empty string
     if(schema[prop].description){
-      schemadescription = schema[prop].description; 
+      schemadescription = schema[prop].description;
     }
     else {
       schemadescription = "";
@@ -1629,18 +1609,6 @@ function traverse(schema, path, resolve, rootSchema) {
       prop = prop.replace("(optional)","");
       copy[prop] = "";
       //copy[prop] = schema[prop];
-=======
-
-  if (Array.isArray(schema)) {
-    copy = [];
-  }
-
-  Object.keys(schema).forEach(function (prop) {
-    if (typeof schema[prop] === 'object' && prop !== 'definitions') {
-      copy[prop] = traverse(schema[prop], path.concat([prop]), resolve, copy);
-    } else {
-      copy[prop] = schema[prop];
->>>>>>> b86fb26ca822d31833b91b1a1f07b0234bf9bd2c
     }
   });
   return copy;
