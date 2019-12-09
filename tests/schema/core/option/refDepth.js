abb474f91abab2f2e1889a9e341277b81554d526
Jacob Levernier <levernierj@email.chop.edu>
2019-12-09 10:35:40 -0500

    Updated tests.

--- tests/schema/core/option/refDepth.js
+++ tests/schema/core/option/refDepth.js
@@ -1,11 +1,12 @@
+const seedrandom = require('seedrandom');
+
 module.exports = {
   register(jsf) {
     return jsf.option({
-      refDepthMin: 6,
-      refDepthMax: 6,
       minItems: 1,
       maxItems: 1,
       alwaysFakeOptionals: true,
+      random: seedrandom('some seed'),
     });
   },
 };
