22c91f697b11d2124bd0cda8d265e5362da88f42
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-08-31 14:21:18 -0500

    Cleanup; WIP

--- src/web/js/lib/Gists.svelte
+++ src/web/js/lib/Gists.svelte
@@ -1,7 +1,6 @@
 <script>
   import { onMount } from 'svelte';
   import { navigateTo } from 'yrv';
-  import Modal from './Modal.svelte';
   import { all, loggedIn } from './gists';
 
   let term = '';
@@ -18,32 +17,30 @@
     || x.description.toLowerCase().includes(term.toLowerCase())
     || Object.keys(x.files).some(k => k.toLowerCase().includes(term.toLowerCase())));
 </script>
-<Modal>
-  <label class="mb flx flx-c nosl">
-    <span>Filter gists:</span>
-    <input class="f txt ml flx-a" type="search" bind:value={term} />
-  </label>
-  {#if $loggedIn}
-    {#if pending}
-      Loading gists...
-    {:else}
-      <ol class="lr zb max">
-        {#each filtered as item}
-          <li class="mb ni">
-            <div class="flx flx-c">
-              <a class="tdn flx-a" target="_blank" href="{item.html_url}">{item.description || item.id}</a>
-              <button class="bu ml nosl" on:click={() => navigateTo(`/#gist/${item.id}`)}>Load gist</button>
-            </div>
-            <ul class="ml lr">
-              {#each Object.entries(item.files) as [file, info]}
-                <li class="ni">
-                  <a class="bl" title="Type: {info.type}" target="_blank" href="{info.raw_url}">{file} &mdash; {(info.size / 1024).toFixed(2)}KB</a>
-                </li>
-              {/each}
-            </ul>
-          </li>
-        {/each}
-      </ol>
-    {/if}
+<label class="mb flx flx-c nosl">
+  <span>Filter gists:</span>
+  <input class="f txt ml flx-a" type="search" bind:value={term} />
+</label>
+{#if $loggedIn}
+  {#if pending}
+    Loading gists...
+  {:else}
+    <ol class="lr zb max">
+      {#each filtered as item}
+        <li class="mb ni">
+          <div class="flx flx-c">
+            <a class="tdn flx-a" target="_blank" href="{item.html_url}">{item.description || item.id}</a>
+            <button class="bu ml nosl" on:click={() => navigateTo(`/#gist/${item.id}`)}>Load gist</button>
+          </div>
+          <ul class="ml lr">
+            {#each Object.entries(item.files) as [file, info]}
+              <li class="ni">
+                <a class="bl" title="Type: {info.type}" target="_blank" href="{info.raw_url}">{file} &mdash; {(info.size / 1024).toFixed(2)}KB</a>
+              </li>
+            {/each}
+          </ul>
+        </li>
+      {/each}
+    </ol>
   {/if}
-</Modal>
+{/if}
