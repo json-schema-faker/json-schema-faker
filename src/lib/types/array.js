4a2cb53add0af584c26011f5fcbb0e9cd16694e3
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-11-30 11:48:03 -0600

    undefined check

--- src/lib/types/array.js
+++ src/lib/types/array.js
@@ -63,13 +63,13 @@ function arrayType(value, path, resolve, traverseCallback) {
 
   if (optionAPI('minItems')) {
     // fix boundaries
-    minItems = !!maxItems
+    minItems = maxItems !== undefined
       ? optionAPI('minItems')
       : Math.min(optionAPI('minItems'), maxItems);
   }
 
   if (optionAPI('maxItems')) {
-    maxItems = !!minItems
+    maxItems = minItems !== undefined
       ? optionAPI('maxItems')
       : Math.min(optionAPI('maxItems'), minItems);
 
