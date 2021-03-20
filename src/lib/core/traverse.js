cadd46184a237b0c78fae337f3648262a4553318
Arnab Gupta <arnab@abacusinsights.com>
2021-03-20 11:03:03 -0400

    Added code to allow null or undefined returned from custom functions

--- src/lib/core/traverse.js
+++ src/lib/core/traverse.js
@@ -59,7 +59,9 @@ function traverse(schema, path, resolve, rootSchema) {
     const retval = utils.typecast(null, schema, () => schema.generate(rootSchema, path));
     const type = typeof retval;
 
-    if (type === schema.type || (type === 'number' && schema.type === 'integer') || (Array.isArray(retval) && schema.type === 'array')) {
+    if (type === schema.type ||
+       (type === 'number' && schema.type === 'integer') ||
+       (Array.isArray(retval) && schema.type === 'array') || !retval) {
       return retval;
     }
   }
