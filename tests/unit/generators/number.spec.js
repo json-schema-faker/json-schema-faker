c3a1893eed3474e1a8651ce84d5da38a31e1c409
Łukasz Kużyński <lukasz@stoplight.io>
2021-09-30 13:57:53 +0200

    fix: 679 properly support numbers in maximum possible ranges

--- tests/unit/generators/number.spec.js
+++ tests/unit/generators/number.spec.js
@@ -8,4 +8,30 @@ describe('Number Generator', () => {
 
     expect(n).not.to.eql(m);
   });
+
+  describe('should ignore min and max if using Number.MAX_VALUE in then', () => {
+    it('ignoring minimum', () => {
+      const n = numberType({ minimum: -Number.MAX_VALUE });
+
+      expect(n).to.be.a('number');
+      expect(n).to.not.be.NaN;
+      expect(n).to.not.be.eq(Infinity);
+    });
+
+    it('ignoring minimum', () => {
+      const n = numberType({ maximum: Number.MAX_VALUE });
+
+      expect(n).to.be.a('number');
+      expect(n).to.not.be.NaN;
+      expect(n).to.not.be.eq(Infinity);
+    });
+
+    it('ignoring both', () => {
+      const n = numberType({ minimum: -Number.MAX_VALUE, maximum: Number.MAX_VALUE });
+
+      expect(n).to.be.a('number');
+      expect(n).to.not.be.NaN;
+      expect(n).to.not.be.eq(Infinity);
+    });
+  });
 });
