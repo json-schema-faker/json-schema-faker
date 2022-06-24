bfac07afe15ee435b0362573fd5d1444b13e266c
Luca Himstedt <luca.himstedt@faible.io>
2022-06-24 12:13:42 +0200

    fix: ensure two digits in clampDate
    Forces all positions in date string to have two digits.

--- src/lib/core/utils.js
+++ src/lib/core/utils.js
@@ -56,8 +56,8 @@ function clampDate(value) {
 
   let [year, month, day] = value.split('T')[0].split('-');
 
-  month = Math.max(1, Math.min(12, month));
-  day = Math.max(1, Math.min(31, day));
+  month = (`0${Math.max(1, Math.min(12, month))}`).slice(-2);
+  day = (`0${Math.max(1, Math.min(31, day))}`).slice(-2);
 
   return `${year}-${month}-${day}`;
 }
@@ -216,7 +216,6 @@ function typecast(type, schema, callback) {
       switch (schema.format) {
         case 'date-time':
         case 'datetime':
-          value = new Date(clampDate(value)).toISOString().replace(/([0-9])0+Z$/, '$1Z');
           value = new Date(clampDateTime(value)).toISOString().replace(/([0-9])0+Z$/, '$1Z');
           break;
 
