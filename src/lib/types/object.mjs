7030fb9ad6034c8efc52553d19a47e6d345aad85
Alvaro Cabrera <pateketrueke@gmail.com>
2023-03-24 03:52:24 -0600

    revert #751

--- src/lib/types/object.mjs
+++ src/lib/types/object.mjs
@@ -14,7 +14,7 @@ function objectType(value, path, resolve, traverseCallback) {
   const properties = value.properties || {};
   const patternProperties = value.patternProperties || {};
   const requiredProperties = typeof value.required === 'boolean' ? [] : (value.required || []).slice();
-  const allowsAdditional = value.additionalProperties !== undefined && value.additionalProperties !== false;
+  const allowsAdditional = value.additionalProperties !== false;
 
   const propertyKeys = Object.keys(properties);
   const patternPropertyKeys = Object.keys(patternProperties);
